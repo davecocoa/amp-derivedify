92af52a280eb962e70d709d8b1c6b783a9769f80
David McMullin <david.mcmullin@gmail.com>
2015-05-07 21:54:25 +0100

    add more lodash

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 var isString = require('lodash.isstring');
 var isObject = require('lodash.isobject');
+var toArray = require('lodash.toarray');
+var constant = require('lodash.constant');
 
 module.exports = function(func){
   return function(){
     var derivedObj = { deps: [] };
-    var args = Array.prototype.slice.call(arguments);
+    var args = toArray(arguments);
     args.forEach(function(arg){
-      // string name, literal or other derived obj
+      // string property name, literal or other derived obj
       if(isString(arg)) { derivedObj.deps.push(arg); }
       if(isObject(arg)) { derivedObj.deps = derivedObj.deps.concat(arg.deps); }
     });
 
     derivedObj.fn = function(){
       var realArgs = args.map(function(arg){
-        // string name, literal or other derived obj
+        // string property name, literal or other derived obj
         if(isString(arg)) { return this[arg]; }
         if(isObject(arg)) { return arg.fn.apply(this); }
         return arg;
@@ -26,5 +28,5 @@ module.exports = function(func){
 };
 
 module.exports.l = function(literal){
-  return { deps: [], fn: function(){ return literal; } };
+  return { deps: [], fn: constant(literal) };
 };
